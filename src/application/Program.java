603db43bb8e355e550fb254499280b60af95069e
sillasconde <sillasconde@usp.br>
2024-06-22 15:50:08 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,8 +23,14 @@ public class Program {
 		System.out.println("================================");
 		System.out.println("TEST findByDepartment:");
 		Department department = new Department(1,null);
-		
 		List<Seller> sellerList = sellerDao.findByDepartment(department);
+		for (Seller sellerExemple: sellerList) {
+			System.out.println(sellerExemple);
+		}
+
+		System.out.println("================================");
+		System.out.println("TEST findAll:");
+		sellerList = sellerDao.findAll();
 		for (Seller sellerExemple: sellerList) {
 			System.out.println(sellerExemple);
 		}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -43,37 +43,36 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		PreparedStatement st = null;
 
+		PreparedStatement st = null;
 		ResultSet rs = null;
 
 		try {
-			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName " + "FROM seller " + "INNER JOIN department "
-							+ "ON seller.DepartmentId = department.Id " + "WHERE " + "department.Id = ? "
-									+ "ORDER BY Name");
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " + "FROM seller "
+					+ "INNER JOIN department " + "ON seller.DepartmentId = department.Id " + "WHERE "
+					+ "department.Id = ? " + "ORDER BY Name");
 
 			st.setInt(1, department.getId());
 			rs = st.executeQuery();
 			List<Seller> sellers = new ArrayList<>();
-			Map<Integer,Department> depMap = new HashMap<>(); 
-			
-			while (rs.next()) {	
+			Map<Integer, Department> depMap = new HashMap<>();
+
+			while (rs.next()) {
 				// Consult depMap for existing department
 				Department dep = depMap.get(rs.getInt("DepartmentId"));
-				
+
 				if (dep == null) {
-					//Add department to Map if exists
+					// Add department to Map if exists
 					dep = instatiateDepartment(rs);
 					depMap.put(rs.getInt("DepartmentId"), dep);
 				}
-				
+
 				Seller seller = instantiateSeller(rs, dep);
 
 				sellers.add(seller);
 
 			}
-			
+
 			return sellers;
 
 		} catch (SQLException e) {
@@ -153,8 +152,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
 
-		return null;
+		try {
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " + "FROM seller "
+					+ "INNER JOIN department " + "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+
+			rs = st.executeQuery();
+			List<Seller> sellers = new ArrayList<>();
+			Map<Integer, Department> depMap = new HashMap<>();
+
+			while (rs.next()) {
+				// Consult depMap for existing department
+				Department dep = depMap.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					// Add department to Map if exists
+					dep = instatiateDepartment(rs);
+					depMap.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller seller = instantiateSeller(rs, dep);
+
+				sellers.add(seller);
+
+			}
+
+			return sellers;
+
+		} catch (SQLException e) {
+
+			throw new DbException(e.getMessage());
+
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }

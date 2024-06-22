ac6bca163ba1d08fa29fabeec3b1674bfd62e92e
sillasconde <sillasconde@usp.br>
2024-06-22 16:08:56 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,7 +14,6 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		
 		System.out.println("================================");
 		System.out.println("TEST findById:");
@@ -34,8 +34,16 @@ public class Program {
 		for (Seller sellerExemple: sellerList) {
 			System.out.println(sellerExemple);
 		}
-
 		
+		System.out.println("================================");
+		System.out.println("TEST insert:");
+		department = new Department(1,null);
+		seller = new Seller(10, "Kleiton", "kleiton@gmail.com", new Date(), 5000.0, department);
+		//sellerDao.insert(seller);
+		System.out.println("Inserted Id:");
+		System.out.println(
+				sellerDao.findById(seller.getId())
+		);	
 		
 		
 	}

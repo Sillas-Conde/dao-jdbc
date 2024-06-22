81f8e43c748d1b872346992d32883fe75fcac255
sillasconde <sillasconde@usp.br>
2024-06-22 14:32:17 -0300

    Create Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +13,12 @@ public class Program {
 		
 		Department dep = new Department(0,"Books");
 		System.out.println(dep);
+		
+		Date birth = new Date();
+		Seller seller = new Seller(0, "Sillas", "sillasconde@outlook.com", birth,2500.0, dep);
+		
+		System.out.println(seller);
+		
 	}
 
 }

fd3715dac481b15cf3b8e6b95ca392d53826071e
sillasconde <sillasconde@usp.br>
2024-06-22 14:45:54 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,9 @@
 package application;
 
-import java.text.DateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +17,9 @@ public class Program {
 		
 		Date birth = new Date();
 		Seller seller = new Seller(0, "Sillas", "sillasconde@outlook.com", birth,2500.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		
 		System.out.println(seller);
 		
